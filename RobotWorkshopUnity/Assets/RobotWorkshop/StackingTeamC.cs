4c2325b84c6746dbadc3f28cbd731a4171e1dfe0
jae <freetime15@daum.net>
2018-01-19 11:48:31 +0000

    JS_Complete

--- RobotWorkshopUnity/Assets/RobotWorkshop/StackingTeamC.cs
+++ RobotWorkshopUnity/Assets/RobotWorkshop/StackingTeamC.cs
@@ -7,10 +7,6 @@ public class StackingTeamC : IStackable
     public string Message { get; set; }
     public IEnumerable<Orient> Display { get { return _placeTiles; } }
 
-    //readonly float _gap = 0.005f;
-    //readonly Vector3 _pickPoint = new Vector3(0.2f, 0, 0.4f);
-   // readonly Vector3 _placePoint = new Vector3(1.2f, 0, 0.4f);
-   // readonly Vector3 _tileSize = new Vector3(0.18f, 0.045f, 0.06f);
     readonly ICamera _camera;
     List<Orient> _placeTiles = new List<Orient>();
 
@@ -41,10 +37,10 @@ public class StackingTeamC : IStackable
         if (_placeTiles.Count == 0)
         {
             // definig place area and scaning
-            var scanRect = new Rect(1.4f * 0.25f + m,   0 + m,  1.4f * 0.75f - m * 2,   0.8f - m * 2); //placing space(1.05m)
+            var scanRect = new Rect(1.4f * 0.25f + m, 0 + m, 1.4f * 0.75f - m * 2, 0.8f - m * 2); //placing space(1.05m)
             var scanTiles = _camera.GetTiles(scanRect);
 
-            var _center = new Vector3(0.7f,0f,0.4f);
+            var _center = new Vector3(0.7f, 0f, 0.4f);
             if (!CheckCamera(scanTiles)) return null;
             if (scanTiles.Count > 1)
             {
@@ -63,57 +59,24 @@ public class StackingTeamC : IStackable
         _placeTiles.RemoveAt(0);
         Message = $"{_placeTiles.Count} tiles remaining in the row";
 
-        return new PickAndPlaceData { Pick = pick, Place = place };  //,Retract=true  // to fulfil brick in real time. but makes error in simulation.
+        return new PickAndPlaceData { Pick = pick, Place = place, Retract = true };   // to fulfil brick in real time. but makes error in simulation.
     }
 
     List<Orient> CreateRow(Orient orient, Vector3 center) //orient is the scanned brick's center
-    {   //float m = 0.02f;
-        //scan Rect = (1.4f * 0.25f + m ,   0 + m,    1.4f * 0.75f - m * 2,   0.8f - m * 2);
-        // pickRect = (0 + m            ,   0 + m,    1.4f * 0.25f - m * 2,   0.8f - m * 2); =vector(1.4 , 0, 0.8)
-        // _pickPoint =(0.2f, 0, 0.4f);
-        //_placePoint = new Vector3(1.2f, 0, 0.4f);
-        // _tileSize = new Vector3(0.18f, 0.045f, 0.06f);
-        //_center = new Vector3(0.7f,0f,0.4f);
-
-        var allBlocks = new List<Orient>();
+    {   var allBlocks = new List<Orient>();
         var radius = Vector3.Distance(orient.Center, center); //radius point oriented - center of orient 
         var tileNumberf = (2 * Mathf.PI * radius) / (0.18f * 1.2f); //Number of Tile
-             int tileNumber = (int)Mathf.Floor(tileNumberf);
+        int tileNumber = (int)Mathf.Floor(tileNumberf);
 
         for (int i = 1; i < tileNumber; i++)
         {
             var angle = 360 / (float)tileNumber;
-            var newTile = orient.RotateAround(center, i*angle); //newTile is the birck's center of new one
+            var newTile = orient.RotateAround(center, i * angle); //newTile is the birck's center of new one
             allBlocks.Add(newTile);
         }
-
-        // even number changes rule.
-        if (scanTiles.Count > 1 )
-        {
-            tileNumber = (int)Mathf.Floor((2 * Mathf.PI * radius) / (0.06f * 1.2f));
-            for (int i = 1; i < tileNumber; i++)
-            {
-                var angle = 360 / (float)tileNumber;
-                var newTile = orient.RotateSecond(center, i * angle); //newTile is the birck's center of new one
-                allBlocks.Add(newTile);
-            }
-        }
-        //
-
-            return allBlocks;
+        return allBlocks;
     }
-
-
-    //for the second rule 
-    public Orient RotateSecond(Vector3 origin, float angle)
-    {
-        var quat = Quaternion.Euler(0, angle, 0);
-        var center = origin + quat * (Center - origin);
-        var rotation = quat * Rotation;
-        return new Orient(center, rotation);
-    }
-
-
+    
 }
 
 class TeamCVirtualCamera : ICamera
@@ -124,14 +87,14 @@ class TeamCVirtualCamera : ICamera
         var t = new[] //new array is about orient
         {
 
-           new Orient(0.8f,0.045f,0.6f,120f),
+           new Orient(0.8f,0.045f,0.6f,90f),
            new Orient(0.8f,0.045f*2,0.6f,30f),
-           new Orient(0.8f,0.045f*3,0.6f,60f),
+           new Orient(0.8f,0.045f*4,0.6f,60f),
         };
         _sequence = new Queue<Orient[]>(new[]
         {
            new[] {t[0]},
-           new[] {t[1]}, 
+           new[] {t[1]},
            new[] {t[1]},
            new[] {t[2]},
            new[] {t[2]},
@@ -140,7 +103,6 @@ class TeamCVirtualCamera : ICamera
            new[] {t[2]},
            new[] {t[2]},
            new[] {t[2]},
-            new[] {t[2]},
            new[] {t[2]},
            new[] {t[2]},
            new[] {t[2]},
@@ -148,15 +110,14 @@ class TeamCVirtualCamera : ICamera
            new[] {t[2]},
            new[] {t[2]},
            new[] {t[2]},
-            new[] {t[2]},
+           new[] {t[2]},
+           new[] {t[2]},
            new[] {t[2]},
            new[] {t[2]},
            new[] {t[2]},
 
            new Orient[0]
         });
-
-        //Number of tiles. number of implementation.  
     }
 
 
@@ -165,5 +126,6 @@ class TeamCVirtualCamera : ICamera
         return _sequence.Dequeue();
     }
 }
+
 
 
